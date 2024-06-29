9b1f19540e8a8f72b992216dc20e2a09804fad3f
eduardomelchor34 <eduardomelchor34@gmail.com>
2024-06-29 03:37:51 +0000

    Pensemos en comoponentes pequenios

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponenet } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
 
 
 @NgModule({
   declarations: [
-    MainPageComponenet
+    MainPageComponenet,
+    ListComponent
   ],
   exports: [
     MainPageComponenet
