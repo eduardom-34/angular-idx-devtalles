2f31a93d54d40472106793cb32919ef6bd0fce43
eduardomelchor34 <eduardomelchor34@gmail.com>
2024-06-29 04:40:28 +0000

    @Input - Recibir del padre

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainPageComponenet } from './pages/main-page.component';
+import { MainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
 import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 @NgModule({
   declarations: [
-    MainPageComponenet,
+    MainPageComponent,
     ListComponent,
     AddCharacterComponent
   ],
   exports: [
-    MainPageComponenet
+    MainPageComponent
   ],
   imports: [
     CommonModule,
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html'
 })
 
-export class MainPageComponenet {
+export class MainPageComponent {
 
+  public characters: Character[] = [{
+    name: 'Krilin',
+    power: 10000
+  }, {
+    name: 'Goku',
+    power: 9500
+  }, {
+    name: 'vegeta',
+    power: 7500
+  }
+];
+;
 
 }
